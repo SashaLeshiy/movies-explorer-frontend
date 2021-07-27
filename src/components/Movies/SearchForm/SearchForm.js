124f38d5bc43b705cc8cf8c02cf31f90b7bf609a
Sasha Leshiy <zomlesh@yandex.ru>
2021-07-27 17:28:06 +0300

    Верстка поиска + чекбокс

--- src/components/Movies/SearchForm/SearchForm.js
+++ src/components/Movies/SearchForm/SearchForm.js
@@ -7,7 +7,13 @@ function SearchForm() {
                 <form name="searchForm" className="searchForm">
                     <input className="searchForm__input" type="text" value="Фильм" />
                     <button className="searchForm__button" type="submit">Поиск</button>
+                    <label className="searchForm__checkbox">
+                        <input type="checkbox" />
+                        <span className="searchForm__checkbox_switch"></span>
+                        <span className="searchForm__checkbox_heading">Короткометражки</span>
+                    </label>
                 </form>
+                
             </div>
         </section>) 
    );
