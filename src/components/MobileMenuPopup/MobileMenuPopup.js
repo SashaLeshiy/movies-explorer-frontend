997d2ea2af9a62a53eb1b3e28e179d8980b88bfb
Sasha Leshiy <zomlesh@yandex.ru>
2021-07-29 18:33:10 +0300

    Добавление стилей для мобильного меню

--- src/components/MobileMenuPopup/MobileMenuPopup.js
+++ src/components/MobileMenuPopup/MobileMenuPopup.js
@@ -4,18 +4,20 @@ import account from '../../images/account_icon.svg';
 function MobileMenuPopup({ linkToMovies, linkToSavedMovies, isMobileMenu, closeMobileMenu }) {
     return (
         <div className={`mobileMenu ${isMobileMenu ? "mobileMenu__open" : ""}`}>
-            <button onClick={closeMobileMenu} className="mobileMenuClose"></button>
-            <ul className="mobileMenu__list">
-                <li className="mobileMenu__list mobileMenu__list_movies">
-                    <button onClick={linkToMovies} className="button">Главная</button>
-                    <button onClick={linkToMovies} className="button">Фильмы</button>
-                    <button onClick={linkToSavedMovies} className="button">Сохраненные фильмы</button>
-                </li>
-                <li className="mobileMenu__list mobileMenu__list_movies_account">
-                    <button className="button">Аккаунт</button>
-                    <button className="button_account"><img src={account} alt="иконка аккаунта" /></button>
-                </li>
-            </ul>
+            <div className="mobileMenu__container">
+                <button onClick={closeMobileMenu} className="mobileMenuClose"></button>
+                <ul className="mobileMenu__list">
+                    <li className="mobileMenu__list mobileMenu__list_movies">
+                        <button onClick={linkToMovies} className="button">Главная</button>
+                        <button onClick={linkToMovies} className="button">Фильмы</button>
+                        <button onClick={linkToSavedMovies} className="button">Сохраненные фильмы</button>
+                    </li>
+                    <li className="mobileMenu__list mobileMenu__list_movies_account">
+                        <button className="button">Аккаунт</button>
+                        <button className="button_account"><img src={account} alt="иконка аккаунта" /></button>
+                    </li>
+                </ul>
+            </div>
         </div>
     );
 }
