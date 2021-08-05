08b6191de5365db8e79e95ea25c8daf59bfd5be7
SashaLeshiy <zomlesh@yandex.ru>
2021-08-05 16:34:42 +0300

    доработка шапки главной страницы

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Promo from './Promo/Promo';
 import AboutProject from './AboutProject/AboutProject';
 import Techs from './Techs/Techs';
 import AboutMe from './AboutMe/AboutMe';
 
 
-function Main() {
+function Main({ setMainPage}) {
+    useEffect(() => {
+        setMainPage(true);
+    })
     return (
         <>
             <Promo />
